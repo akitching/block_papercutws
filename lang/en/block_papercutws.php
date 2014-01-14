62f9ba586ce91c34b35a449f027e10efd2431af3
Alex Kitching <ak@stowmarketmiddle.suffolk.sch.uk>
2014-01-14 11:43:37 +0000

    Add missing strings to language file

--- lang/en/block_papercutws.php
+++ lang/en/block_papercutws.php
@@ -3,3 +3,14 @@ $string['pluginname'] = 'PaperCut NG WebService block';
 $string['papercutws'] = 'PaperCut NG WebService';
 $string['papercutws:addinstance'] = 'Add a new PaperCut NG WebService block';
 $string['papercutws:myaddinstance'] = 'Add a new PaperCut NG WebService block to the My Moodle page';
+
+$string['title'] = 'Block Title';
+$string['title_desc'] = 'Please enter the title for the block.';
+$string['authtoken'] = 'Authtoken';
+$string['authtoken_desc'] = 'Please enter the PaperCut server\'s authtoken';
+$string['serveruri'] = 'Server URL';
+$string['serveruri_desc'] = 'Please enter the URL of the PaperCut server.';
+$string['port'] = 'Server Port';
+$string['port_desc'] = 'Which port is the PaperCut server listening on?';
+$string['https'] = 'Use HTTPS';
+$string['https_desc'] = 'Use HTTPS to access the PaperCut Server?';
